3a7102bbccb3cf4d79231e23c69ddc92c45fcf0b
Lukasz Samson <lukaszsamson@gmail.com>
2017-05-28 09:15:54 +0200

    test assets and main page skeleton

--- src/App.elm
+++ src/App.elm
@@ -1,6 +1,7 @@
 module App exposing (..)
 
-import Html exposing (Html, div, text, h1, p, button)
+import Css
+import Html exposing (Html, a, button, div, h1, img, p, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
@@ -34,14 +35,39 @@ update message model =
 -- view : Model -> Html Msg
 
 
+styles =
+    Css.asPairs >> Html.Attributes.style
+
+
+navItem caption link imageUrl className =
+    div [ class className ]
+        [ a [ href link ]
+            [ img [ src imageUrl, styles [ Css.width (Css.pct 100) ] ] []
+            , p [] [ text caption ]
+            ]
+        ]
+
+
+mainContent =
+    div [ styles [ Css.marginTop (Css.px 100) ] ]
+        [ h1
+            [ styles
+                [ Css.backgroundColor (Css.rgb 74 153 120)
+                , Css.height (Css.px 250)
+                ]
+            ]
+            [ text "Wellcome in da shop" ]
+        , div [ class "content" ]
+            [ navItem "trololo" "#" "img/1.jpg" "nav1"
+            , navItem "bla" "#" "img/2.jpg" "nav2"
+            , navItem "bla" "#" "img/3.jpg" "nav3"
+            , navItem "bla" "#" "img/4.jpg" "nav4"
+            ]
+        ]
+
+
+view : Model -> Html Msg
 view model =
     div [ class "container" ]
-        [ h1 [] [ text "Hot loading" ]
-        , p [] [ text "Click on the button below to increment the state. ccThen make a change to the source code and see how the updated state is retained after you recompile." ]
-        , p [] [ text <| toString model ]
-        , button
-            [ class "btn btn-primary"
-            , onClick Inc
-            ]
-            [ text "+ 1" ]
+        [ mainContent
         ]
