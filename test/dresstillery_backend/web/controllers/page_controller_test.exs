de5590491650828a6418fce75c899fe87be99c5c
Lukasz Samson <lukaszsamson@gmail.com>
2017-07-20 09:58:56 +0200

    test fix

--- test/dresstillery_backend/web/controllers/page_controller_test.exs
+++ test/dresstillery_backend/web/controllers/page_controller_test.exs
@@ -3,6 +3,11 @@ defmodule DresstilleryBackend.Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Dresstillery"
+  end
+
+  test "GET /admin", %{conn: conn} do
+    conn = get conn, "/admin"
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
