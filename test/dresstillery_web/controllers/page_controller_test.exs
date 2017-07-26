3fe9cf850c2dbaafe4874556cb621a46b27cb9dd
Lukasz Samson <lukaszsamson@gmail.com>
2017-07-27 01:33:09 +0200

    tests added

--- test/dresstillery_web/controllers/page_controller_test.exs
+++ test/dresstillery_web/controllers/page_controller_test.exs
@@ -6,8 +6,30 @@ defmodule DresstilleryWeb.PageControllerTest do
     assert html_response(conn, 200) =~ "Dresstillery"
   end
 
+  test "GET /client_route", %{conn: conn} do
+    conn = get conn, "/client_route"
+    assert html_response(conn, 200) =~ "Dresstillery"
+  end
+
+  test "GET /non_existing.jpg", %{conn: conn} do
+    assert_error_sent 404, fn ->
+      get conn, "/non_existing.jpg"
+    end
+  end
+
+  test "GET /js/app.js", %{conn: conn} do
+    conn = get conn, "/js/app.js"
+    assert response(conn, 200) =~ "hasOwnProperty"
+  end
+
   test "GET /admin", %{conn: conn} do
-    conn = get conn, "/admin"
+    conn = get conn, page_path(conn, :index)
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+
+  test "GET /admin/non_existing", %{conn: conn} do
+    assert_error_sent 404, fn ->
+      get conn, page_path(conn, :index) <> "/non_existing"
+    end
   end
 end
