08e146439a410eb8d2431aa5a5a50f4011562550
Lukasz Samson <lukaszsamson@gmail.com>
2017-07-28 19:49:56 +0200

    session control done

--- lib/dresstillery_web/views/layout_view.ex
+++ lib/dresstillery_web/views/layout_view.ex
@@ -1,3 +1,19 @@
 defmodule DresstilleryWeb.LayoutView do
   use DresstilleryWeb, :view
+  import Plug.Conn
+
+  def can_logout(conn) do
+    cond do
+      get_session(conn, :tfa_user) -> true
+      get_session(conn, :current_user) -> true
+      true -> false
+    end
+  end
+
+  def can_change_password(conn) do
+    cond do
+      get_session(conn, :current_user) -> true
+      true -> false
+    end
+  end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+permissions = Dresstillery.Administration.BackofficePermissions.all
+|> Enum.map(& %Dresstillery.Administration.Permission{name: &1})
+
+# Comeonin.Bcrypt.hashpwsalt("p@ssw0rd")
+%Dresstillery.Administration.BackofficeUser{login: "admin", password: "$2b$12$R3nBGCoa53vS4M1XgBpTgu7LROinWVaYEhKLG0Xo77Nghx1DtRVu."}
+|> Ecto.Changeset.change()
+|> Ecto.Changeset.put_embed(:permissions, permissions)
+|> Dresstillery.Repo.insert!
