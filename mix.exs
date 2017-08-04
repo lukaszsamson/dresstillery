ff444af732f3c8abed882ed5530e1f3bc0c8d872
Lukasz Samson <lukaszsamson@gmail.com>
2017-08-04 23:26:04 +0200

    back to stable

--- mix.exs
+++ mix.exs
@@ -34,8 +34,8 @@ defmodule Dresstillery.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      # {:phoenix, "~> 1.3.0"},
-      {:phoenix, github: "phoenixframework/phoenix", override: true},
+      {:phoenix, "~> 1.3.0"},
+      # {:phoenix, github: "phoenixframework/phoenix", override: true},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
